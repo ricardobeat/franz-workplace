ffc5543d8dd34b18c17a30e943cccd00a48b4444
Ricardo Tomasi <ricardobeat@gmail.com>
2016-12-22 23:52:01 +0100

    Fix notification script

--- workplace/webview.js
+++ workplace/webview.js
@@ -1,8 +1,13 @@
 module.exports = (Franz, options) => {
+
+  document.querySelector('#pagelet_bluebar').style.display = "none"
+
   function getMessages() {
-    const title =  document.querySelector("title").textContent.match(/\d+/);
-    var count = title !== null ? title[0] : 0;
-    Franz.setBadge(count);
+    const title =  document.querySelector("title")
+    const matches = title && title.textContent.match(/\d+/)
+    const count = matches && matches[0] || 0
+    Franz.setBadge(count)
   }
-  Franz.loop(getMessages);
+
+  Franz.loop(getMessages)
 }
