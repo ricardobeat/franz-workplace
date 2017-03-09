5718f2b0bdd8f4e021f93dd2f52b96cb2c99c742
Ricardo Tomasi <ricardobeat@gmail.com>
2017-03-09 13:47:54 +0100

    naive notification implementation

--- workplace/webview.js
+++ workplace/webview.js
@@ -2,14 +2,36 @@ const path = require('path')
 
 module.exports = (Franz, options) => {
 
-  document.querySelector('#pagelet_bluebar').style.display = "none"
+  var unread = 0
 
-  function getMessages() {
-    const title =  document.querySelector("title")
-    const matches = title && title.textContent.match(/\d+/)
-    const count = matches && matches[0] || 0
-    Franz.setBadge(count)
-  }
+  const get = (obj, key) => key.split('.').reduce((o,k,i) => o && o[k], obj)
+
+  // dumb notification implementation. adds '1+' badge for any kind
+  // of 'NewMessage' received, badge is cleared on focus
+
+  ;(function(send) {
+    XMLHttpRequest.prototype.send = function (data) {
+      this.addEventListener("load", function () {
+        if (/chat\.facebook\.com\/pull/.test(this.responseURL)) {
+          try {
+            var res = this.responseText.replace('for (;;); ', '').split(/[\n\r]+/);
+            res.forEach(function(r){
+              var data = JSON.parse(r)
+              if (get(data, 'ms.0.delta.class') == "NewMessage") {
+                Franz.setBadge(unread = '1+')
+              }
+            })
+          } catch (e) {}
+        }
+      });
+      send.call(this, data)
+    }
+  })(XMLHttpRequest.prototype.send)
+
+
+  window.addEventListener('focus', function (e){
+    Franz.setBadge(unread = 0)
+  }, false);
 
   Franz.injectCSS(path.join(__dirname, 'workplace.css'))
 }
