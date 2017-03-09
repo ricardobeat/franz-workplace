dfc9294fee13de6d70b380d352893386991c19f4
Esteban Beltran <esteban.beltran@booking.com>
2017-03-09 14:30:00 +0100

    Add highlight for mentions

--- workplace/webview.js
+++ workplace/webview.js
@@ -1,8 +1,9 @@
 const path = require('path')
+const observeDom = require('./observe-dom');
 
 module.exports = (Franz, options) => {
 
-  var unread = 0
+  var unread = 0, name, interval = null;
 
   const get = (obj, key) => key.split('.').reduce((o,k,i) => o && o[k], obj)
 
@@ -28,10 +29,35 @@ module.exports = (Franz, options) => {
     }
   })(XMLHttpRequest.prototype.send)
 
+  function highlightMentions() {
+    var messages = document.querySelector('[aria-label="Messages"]').querySelectorAll('[data-tooltip-position="left"] span')
+    var regex = new RegExp("(.*)" + name + "(.*)", "i");
+    var msg;
+    for(var i = 0; i < messages.length; i++) {
+      msg = messages[i];
+      if(regex.test(msg.innerHTML)) {
+        msg.classList.add('f-mention');
+      }
+    }
+  }
+
+  // Different init times due to render variations, need to make sure the elements exist
+  function initHighlight() {
+    var chatLog = document.querySelector('[aria-label="Messages"]');
+    if (chatLog) {
+      name = document.querySelector('[title=Profile] span').innerText.toLowerCase();
+      observeDom(chatLog, highlightMentions);
+      highlightMentions();
+      window.clearInterval(interval);
+    }
+  }
 
   window.addEventListener('focus', function (e){
     Franz.setBadge(unread = 0)
   }, false);
+  window.addEventListener('load', function(){
+    interval = window.setInterval(initHighlight, 150);
+  });
 
   Franz.injectCSS(path.join(__dirname, 'workplace.css'))
 }
