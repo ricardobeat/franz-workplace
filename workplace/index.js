9d8e23bd93ec76d874ee87b995a69f10c2e07778
Ricardo Tomasi <ricardobeat@gmail.com>
2017-03-09 13:40:06 +0100

    wip

--- workplace/index.js
+++ workplace/index.js
@@ -1,1 +1,10 @@
-module.exports = Franz => Franz;
+module.exports = Franz => {
+	console.log(Franz)
+	var webViewEl = document.getElementById("webview"); 
+	console.log(webViewEl)
+	webViewEl.addEventListener("did-get-response-details", function(details) {
+	  console.log(details); 
+	});
+
+	return Franz;
+};
